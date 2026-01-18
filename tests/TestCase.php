1e09e0b1b9e01dfea726c7cefaa3e1cc60e3d0ac
Paulo Castellano <paulofcastellano@gmail.com>
2026-01-18 19:05:34 -0300

    feat: working on ci/cd

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Indicates whether the default seeder should run before each test.
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
 }
